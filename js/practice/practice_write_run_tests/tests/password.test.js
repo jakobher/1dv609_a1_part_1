1b4c9075434777103cebe0bb31ccacce2e6fcbdc
Jakob Hermansson <jh225ud@student.lnu.se>
2025-11-24 14:52:22 +0100

    Added tests for trim, hash return type and isPasswordSame

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,9 +1,9 @@
 
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
+// import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugDoesNotTrim'
-// import { Password } from '../src/BugisPasswordAlwaysSame'
+import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
 // import { Password } from '../src/BugNeverContainsNumbers'
@@ -15,11 +15,31 @@ import { Password } from '../src/BugDoesNotHash'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
-    const emptyPassword = '';
-
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+        const validPassword = 'Password12345'
+        const passwordWithSpaces = ' Password12345 '
+        const differentPassword = 'Password23456'
 
     //Add your tests here
+    test('simpleHash should return a number', () => {
+        const pw = new Password(validPassword)
+
+        expect(typeof pw.getPasswordHash()).toBe('number')
+    })
+
+    test('constructor should trim whitespace', () => {
+        const pw1 = new Password(validPassword)
+        const pw2 = new Password(passwordWithSpaces)
+
+        expect(pw1.getPasswordHash()).toBe(pw2.getPasswordHash())
+    })
+
+    test('isPasswordSame should return false for different passwords', () => {
+        const pw3 = new Password(validPassword)
+        const pw4 = new Password(differentPassword)
+        
+        expect(pw3.isPasswordSame(pw4)).toBe(false)
+    })
+
+
+
 });
