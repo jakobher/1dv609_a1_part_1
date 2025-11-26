a6732a3d92262278851962a613509e9000636884
Jakob Hermansson <jh225ud@student.lnu.se>
2025-11-26 14:43:55 +0100

    Complete SSNhelper tests with bug matrix

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,32 +1,18 @@
-import { SSNHelper } from '../src/correct/SSNHelper'; 
-
+// import { SSNHelper } from '../src/correct/SSNHelper' 
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowMonth0'
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat'
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperMessyLuhn'
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperWrongLength'
+import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowMonth13'
 
 
 describe('SSNHelper Tests', () => {
-    
-
-    test('isCorrectLength Should Return True For 11 Characters', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.isCorrectLength('930101-1234')).toBe(true)
-    })
-
-    test('isCorrectLength Should Return False for 10 Characters', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.isCorrectLength('930101-123')).toBe(false)
-    })
 
     test('isCorrectLength Should Return False For 12 Characters', () => {
         const SSN = new SSNHelper()
 
         expect(SSN.isCorrectLength('930101-12345')).toBe(false)
-    })
-
-    test('isCorrectFormat Should Return True For 11 Characters In Correct Format', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.isCorrectFormat('930101-1234')).toBe(true)
     })
 
     test('isCorrectFormat Should Return False For 11 Characters Without -', () => {
@@ -35,16 +21,10 @@ describe('SSNHelper Tests', () => {
         expect(SSN.isCorrectFormat('93010111234')).toBe(false)
     })
 
-        test('isCorrectFormat Should Return False For 11 Characters With A Letter', () => {
+    test('isCorrectFormat Should Return False For 11 Characters With A Letter', () => {
         const SSN = new SSNHelper()
 
         expect(SSN.isCorrectFormat('X3010111234')).toBe(false)
-    })
-
-    test('isValidMonth Should Return True For 05 As Input', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.isValidMonth('05')).toBe(true)
     })
 
     test('isValidMonth Should Return False For 00 As Input', () => {
@@ -59,33 +39,15 @@ describe('SSNHelper Tests', () => {
         expect(SSN.isValidMonth('13')).toBe(false)
     })
 
-    test('isValidDay Should Return True For 15 As Input', () => {
+    test('isValidDay Should Return True For 31 As Input', () => {
         const SSN = new SSNHelper()
 
-        expect(SSN.isValidDay('15')).toBe(true)
-    })
-
-    test('isValidDay Should Return False For 32 As Input', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.isValidDay('32')).toBe(false)
-    })
-
-     test('isValidDay Should Return False For 00 As Input', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.isValidDay('00')).toBe(false)
+        expect(SSN.isValidDay('31')).toBe(true)
     })
 
     test('luhnisCorrect Should Return True With Valid SSN', () => {
         const SSN = new SSNHelper()
 
         expect(SSN.luhnisCorrect('930101-1236')).toBe(true)
-    })
-
-    test('luhnisCorrect Should Return False With Non Valid SSN', () => {
-        const SSN = new SSNHelper()
-
-        expect(SSN.luhnisCorrect('930101-1234')).toBe(false)
     })
 });
