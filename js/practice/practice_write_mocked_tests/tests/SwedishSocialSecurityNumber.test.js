a9455234399ce35f4c9316b892414677818c1ecd
Jakob Hermansson <jh225ud@student.lnu.se>
2025-11-26 16:14:53 +0100

    Add mock test for NoTrim

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,16 +1,16 @@
 import { jest } from '@jest/globals'
 // import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
-import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
+import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
 
 
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
 
     test('constructor Should Throw Exception For Incorrect Length', () => {
         const mockHelper = {
-            isNotCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectLength: jest.fn().mockReturnValue(false),
             isCorrectFormat: jest.fn().mockReturnValue(true),
             isValidMonth: jest.fn().mockReturnValue(true),
             isValidDay: jest.fn().mockReturnValue(true),
@@ -20,6 +20,20 @@ describe('SwedishSocialSecurityNumber Tests', () => {
         expect(() => {
             new SwedishSocialSecurityNumber('123', mockHelper)
         }).toThrow('To short, must be 11 characters')
+    })
+
+    test('constructor Should Trim Input From Whitespaces', () => {
+        const mockHelper = {
+            isCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn().mockReturnValue(true),
+            isValidDay: jest.fn().mockReturnValue(true),
+            luhnisCorrect: jest.fn().mockReturnValue(true)
+        }
+        
+        const ssn = new SwedishSocialSecurityNumber('  930101-1236  ', mockHelper)
+        
+        expect(ssn.getYear()).toBe('93')
     })
 
 });
