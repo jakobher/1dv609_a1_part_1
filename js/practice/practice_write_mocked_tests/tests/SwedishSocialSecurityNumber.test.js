ee3893c003d98feff9d6a3230e27a7425f182e29
Jakob Hermansson <jh225ud@student.lnu.se>
2025-11-26 16:23:59 +0100

    Add mock test for NoLuhn

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,7 +1,8 @@
 import { jest } from '@jest/globals'
-// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
+import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'
 // import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoLenCheck'
-import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecurityNumberNoTrim'
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberNoLuhn'
 
 
 
@@ -34,6 +35,20 @@ describe('SwedishSocialSecurityNumber Tests', () => {
         const ssn = new SwedishSocialSecurityNumber('  930101-1236  ', mockHelper)
         
         expect(ssn.getYear()).toBe('93')
+    })
+
+    test('constructor Should Throw Exception For Invalid SSN', () => {
+        const mockHelper = {
+            isCorrectLength: jest.fn().mockReturnValue(true),
+            isCorrectFormat: jest.fn().mockReturnValue(true),
+            isValidMonth: jest.fn().mockReturnValue(true),
+            isValidDay: jest.fn().mockReturnValue(true),
+            luhnisCorrect: jest.fn().mockReturnValue(false)
+        }
+
+        expect(() => {
+            new SwedishSocialSecurityNumber('930101-1234', mockHelper)
+        }).toThrow("Invalid SSN according to Luhn's algorithm")
     })
 
 });
