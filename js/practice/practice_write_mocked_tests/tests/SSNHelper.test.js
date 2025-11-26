fc6aa0641315fc21e3b67f442d1e6fcd9a2a2697
Jakob Hermansson <jh225ud@student.lnu.se>
2025-11-26 12:13:35 +0100

    Add tests for isCorrectFormat

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -23,4 +23,22 @@ describe('SSNHelper Tests', () => {
         expect(SSN.isCorrectLength('930101-12345')).toBe(false)
     })
 
+    test('isCorrectFormat Should Return True For 11 Characters In Correct Format', () => {
+        const SSN = new SSNHelper()
+
+        expect(SSN.isCorrectFormat('930101-1234')).toBe(true)
+    })
+
+    test('isCorrectFormat Should Return False For 11 Characters Without -', () => {
+        const SSN = new SSNHelper()
+
+        expect(SSN.isCorrectFormat('93010111234')).toBe(false)
+    })
+
+        test('isCorrectFormat Should Return False For 11 Characters With A Letter', () => {
+        const SSN = new SSNHelper()
+
+        expect(SSN.isCorrectFormat('X3010111234')).toBe(false)
+    })
+
 });
